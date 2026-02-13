ded9547424e9a74d519be7b8ecdb21d9cce265b2
Horakal <felipe.reis.e@gmail.com>
2026-02-13 16:13:42 -0300

    Feat: user service and dtos

---
+++ RestApiCase.Application/User/DTOs/Requests/RefreshAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiCase.Application.User.DTOs.Requests
+{
+    public class RefreshAccess
+    {
+        public string RefreshToken { get; set; }
+        public string UserId { get; set; }
+    }
+}
--- RestApiCase.Application/User/Service/UserService.cs
+++ RestApiCase.Application/User/Service/UserService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using RestApiCase.Domain.Commons;
+using RestApiCase.Domain.User.Entities;
 using RestApiCase.Domain.User.Interface;
 
 namespace RestApiCase.Application.User.Service
@@ -22,9 +24,55 @@ namespace RestApiCase.Application.User.Service
             return await _userRepository.Authenticate(name, password);
         }
 
+        public async Task CreateToken(RefreshToken token)
+        {
+            await _userRepository.SaveToken(token);
+        }
+
         public ClaimsPrincipal? GetCurrentUser(string token)
         {
             return Thread.CurrentPrincipal as ClaimsPrincipal;
+        }
+
+        public async Task<RestApiCase.Domain.User.Entities.User?> GetById(Guid userId)
+        {
+            return await _userRepository.GetUser(userId);
+        }
+
+        public async Task<List<Role>> GetRoles(Guid id)
+        {
+            return await _userRepository.GetByIdWithRolesAsync(id);
+        }
+
+        public async Task<bool> ValidateToken(string token, Guid userId)
+        {
+            var refToken = await _userRepository.GetToken(userId);
+            if (refToken is not null && refToken.IsExpired())
+            {
+                await _userRepository.DeleteToken(refToken);
+            }
+            else if (refToken is not null && !refToken.IsExpired())
+            {
+                if (string.Compare(refToken.Token, token) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public async Task DeleteToken(Guid userId)
+        {
+            var refToken = await _userRepository.GetToken(userId);
+            if (refToken is not null)
+            {
+                await _userRepository.DeleteToken(refToken);
+            }
+        }
+
+        public async Task<RefreshToken?> GetRefreshToken(Guid userId)
+        {
+            return await _userRepository.GetToken(userId);
         }
     }
 }
